b9d4b624accf9255264c5cb9920e40130717ac52
robbinl <robbinl@telusplanet.net>
2023-01-13 14:46:53 -0700

    a

--- bc-js-refresh/review-astronauts-example-COMPLETE/js/api/astronaut.js
+++ bc-js-refresh/review-astronauts-example-COMPLETE/js/api/astronaut.js
@@ -1,6 +1,6 @@
 const BASE_URL = "https://lldev.thespacedevs.com/2.2.0";
 
-const getAstronautList = () => {
+const getAstronautList1 = () => {
 	return fetch(`${BASE_URL}/astronaut/`)
 	  .then((response)=> {
 		  return response.json();
@@ -8,6 +8,19 @@ const getAstronautList = () => {
 		.then((data)=> {
 		  return data;
 	  });
+}
+
+const getAstronautList = async () => {
+	// fetch() returns a promise to res.
+	// The await keyword "stalls" the JS assignment
+	// until the data or error is returned (promise resolves or is rejected). 
+	const res = await fetch(`${BASE_URL}/astronaut/`);
+	if (!res.ok) {
+		throw new Error('OPPS');
+	 }
+	// res.json() also returns a promise to data.
+	const data = await res.json();
+	return data;
 }
 
 export { getAstronautList }
